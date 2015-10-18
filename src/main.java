9b1f39176adb3b32d929fcd6e628b460046efb31
Jacek Mackowski <jacekm123@gmail.com>
2015-10-18 11:31:48 +0200

    initial project version

---
+++ src/main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		   FileReader fr = null;
+		   String linia = "";
+
+		   // OTWIERANIE PLIKU:
+		   try {
+		     fr = new FileReader("plik.csv");
+		   } catch (FileNotFoundException e) {
+		       System.out.println("B£¥D PRZY OTWIERANIU PLIKU!");
+		       System.exit(1);
+		   }
+
+		   BufferedReader bfr = new BufferedReader(fr);
+		   // ODCZYT KOLEJNYCH LINII Z PLIKU:
+		   try {
+		     while((linia = bfr.readLine()) != null){
+		        System.out.println(linia);
+		     }
+		    } catch (IOException e) {
+		        System.out.println("B£¥D ODCZYTU Z PLIKU!");
+		        System.exit(2);
+		   }
+
+		   // ZAMYKANIE PLIKU
+		   try {
+		     fr.close();
+		    } catch (IOException e) {
+		         System.out.println("B£¥D PRZY ZAMYKANIU PLIKU!");
+		         System.exit(3);
+		        }
+
+	}
+
+}
